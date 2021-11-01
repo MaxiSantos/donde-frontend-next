7d6406cc5e7e66dd361219700f2036543fec0b0c
maxi-code <max_011_1@hotmail.com>
2021-11-01 13:00:20 -0300

    support for cloudinary in nextjs config.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'res.cloudinary.com'
+    ],
+},
   webpack: (config, { buildId, dev }) => {
     config.resolve.symlinks = false
     return config
