2bda32fca2e300aa5f1ca71185d598b6df73a990
maxi-code <max_011_1@hotmail.com>
2022-08-04 11:54:00 -0300

    updated comming soon text on notification page

--- app/components/pageTemplate/Notification/index.tsx
+++ app/components/pageTemplate/Notification/index.tsx
@@ -2,6 +2,6 @@ import { Typography } from '@mui/material';
 
 export default function Notification() {
   return (
-    <Typography variant="h4">Comming soon</Typography>
+    <Typography variant="h4">En construccion 🚧</Typography>
   )
 }
