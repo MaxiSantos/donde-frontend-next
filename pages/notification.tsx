8a23961bf4eecc0a6b07435f4757c309cab98032
maxi-code <max_011_1@hotmail.com>
2022-09-14 00:14:40 -0300

    perf: :fire:

--- pages/notification.tsx
+++ pages/notification.tsx
@@ -1,7 +1,6 @@
 import { Default } from 'app/common/components/layouts/default';
 import { TranslationHelper } from 'app/common/lib/translation';
 import Notification from 'app/components/pageTemplate/Notification';
-import { NotificationSearch } from 'app/components/pageTemplate/Notification/notificationSearch';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const App = () => (
