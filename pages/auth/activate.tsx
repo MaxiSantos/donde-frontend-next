4a6f2f6ccbbdd6482243799f0ad610353408d07a
maxi-code <max_011_1@hotmail.com>
2022-09-19 11:44:42 -0300

    refactor: :construction: wip for activate page and linking to next-auth flow

--- pages/auth/activate.tsx
+++ pages/auth/activate.tsx
@@ -1,13 +1,27 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import Activate from '../../app/common/pages/auth/activate'
+import Activate from 'app/common/pages/auth/activate'
 import { TranslationHelper } from 'app/common/lib/translation'
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from 'pages/api/auth/[...nextauth]';
 
 const Index: NextPage = () => {
   return <Activate />;
 }
 
-export async function getStaticProps(context) {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const {req, res, query} = context
+  const session = await unstable_getServerSession(req, res, authOptions);
+  const { p = '/'} = query;
+  console.log({session})
+  if(session){
+    return {
+      redirect: {
+        destination: p.toString(),
+        permanent: false
+      }
+    }
+  }
   return {
     props: {
       protected: false,
