84cae163a37d46cf70e3c9da48c0bc9d34e710ef
maxi-code <max_011_1@hotmail.com>
2022-09-22 03:19:50 -0300

    fix: :rotating_light: removing console.log for request because in staging there was a 500 error code complaing it can access cache from request

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,17 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import {SignJWT, jwtVerify, type JWTPayload} from 'jose';
+import {jwtVerify, type JWTPayload} from 'jose';
 
 export async function middleware(request: NextRequest) {  
   const accessToken = request.cookies.get('access-token');
   const refreshToken = request.cookies.get('refresh-token');
   
-  console.log({request})
   if (!accessToken || !refreshToken) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
   try {
-    const { payload } = await jwtVerify(accessToken, new TextEncoder().encode(process.env.APP_SECRET));
+    await jwtVerify(accessToken, new TextEncoder().encode(process.env.APP_SECRET));
     return NextResponse.next();
   } catch (error) {
     console.log('session terminated')
