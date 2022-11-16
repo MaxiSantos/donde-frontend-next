6d3b5bb43709b645eb6f3d1b60679b90c31c4c0a
maxi-code <max_011_1@hotmail.com>
2022-11-16 19:53:22 -0300

    refactor: :recycle: renamed api namem for logging stuffs because they can only be logged in edge enviorment

--- pages/api/log.ts
+++ pages/api/log.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log("my handler")
+  console.log("logging handler")
+  console.log(req?.body)
   res.status(200).json({
     body: req.body,
     query: req.query,
