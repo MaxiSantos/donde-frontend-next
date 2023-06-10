4e6eccc9c33b36381d1cf80db1e6a3f6f77360e7
maxi-code <max_011_1@hotmail.com>
2023-06-10 03:49:45 -0300

    feat: :sparkles: improved revalidate api so getPath is outisde this api

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,38 +1,22 @@
-import { locales } from 'app/shared/config/translationsLocales';
+import { getPaths } from 'app/common/lib/revalidate';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getHook } from "react-hooks-outside";
+
 /*
  * https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration#using-on-demand-revalidation
  */
-const t = getHook("translation");
-const getRoute = (route) => {
-  return route;
-}
-
-const getPaths = (route, slug) => {
-  return locales.map(lang => {
-    return `/${lang}/${getRoute(route)}/${slug}`;
-  })
-}
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Check for secret to confirm this is a valid request
   if (req.method === 'POST') {
-    const { token, route, slug } = req.headers;
+    const { token, routetorevalidate, id } = req.headers;
+    // Check for secret to confirm this is a valid request
     if (token === process.env.VERCEL_API_TOKEN) {
-      // TODO: we should log this somewhere
       try {
-        // this should be the actual path not a rewritten path
-        // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-        const paths = getPaths(route, slug);
-        const apiCall = await Promise.all(paths.map(path => {
+        const pathsToRevalite = await getPaths(routetorevalidate, id);
+        const apiCall = await Promise.all(pathsToRevalite.map(path => {
           return res.revalidate(path as string)
         }));
         apiCall.forEach((data, i) => {
-          console.log("revalidation went ok: ", paths[i])
+          console.log("revalidation went ok: ", pathsToRevalite[i])
         });
-        //await res.revalidate(path as string)
-
         return res.json({ revalidated: true })
       } catch (err) {
         // If there was an error, Next.js will continue
@@ -41,6 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(500).send('Error revalidating')
       }
     } else {
+      console.log("Invalid token")
       return res.status(401).json({ message: "Invalid token" })
     }
   }
