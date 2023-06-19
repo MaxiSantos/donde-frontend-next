0549f357c35e4330ea47d8421592d926cdb52389
maxi-code <max_011_1@hotmail.com>
2023-06-18 21:17:43 -0300

    refactor: :recycle: updated revalidate api to get values from body instead of header.

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,3 +1,4 @@
+import { runCors } from 'app/common/lib/api/helper';
 import { getPaths } from 'app/common/lib/revalidate';
 import { NextApiRequest, NextApiResponse } from 'next';
 
@@ -5,12 +6,20 @@ import { NextApiRequest, NextApiResponse } from 'next';
  * https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration#using-on-demand-revalidation
  */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // TODO: must test if this work in middleware. Maybe do something like if route.startsWith("/api") then ...
+  await runCors(req, res)
   if (req.method === 'POST') {
-    const { token, routetorevalidate, id } = req.headers;
+    console.log({ req })
+    const { token } = req.headers;
+    const { routeToRevalidate, id } = req.body;
+    console.log({ routeToRevalidate })
+    console.log({ id })
+    console.log({ token })
     // Check for secret to confirm this is a valid request
     if (token === process.env.VERCEL_API_TOKEN) {
       try {
-        const pathsToRevalite = await getPaths(routetorevalidate, id);
+        const pathsToRevalite = await getPaths(routeToRevalidate, id);
+        console.log({ pathsToRevalite })
         const apiCall = await Promise.all(pathsToRevalite.map(path => {
           return res.revalidate(path as string)
         }));
