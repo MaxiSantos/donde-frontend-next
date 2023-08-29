1bb1583cbf2bbcefde222794fbc1c4b682bb6d14
maxi-code <max_011_1@hotmail.com>
2023-08-29 19:04:03 -0300

    feat: :sparkles: redesigned welcome page by adding framero-motio and parallax

---
+++ app/components/pageTemplate/Home/Description/index.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Container, Divider, Box } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
+import { Title } from "../Typography";
+import { Trans, useTranslation } from "next-i18next";
+import { TextHelper } from "app/common/lib/text";
+import { MotionWrapper } from "app/common/components/hoc";
+import Detail from "./Detail";
+import { randomColors } from "app/common/constants";
+
+// fixed an issue with ownserState in frid2 mui
+// https://github.com/mui/material-ui/issues/35643#issuecomment-1366206059
+const BoxWrapper = React.forwardRef(function LinkWrapper(props, ref) {
+  // @ts-ignore
+  const { ownerState, ...other } = props;
+  return <Box {...other} ref={ref} />;
+});
+
+type DetailsProp = {
+  icon: {
+    type: string
+    color: string
+  }
+  title?: () => string
+  content?: () => React.ReactNode
+}
+
+const Features = (props) => {
+  const { t } = useTranslation('common');
+  const randomColorLength = randomColors.length;
+  const random = Math.floor(Math.random() * randomColorLength);
+  const getNextCircularIndex = (currentIndex: number): number => {
+    console.log({ currentIndex })
+    return (currentIndex + 1) % randomColorLength;
+  }
+  const details: DetailsProp = [
+    {
+      icon: {
+        type: "im-icon-Clothing-Store",
+        // by adding 80 at the end we are specifying the alpha color value
+        color: (i) => `${randomColors[getNextCircularIndex(i)]}80`
+      }
+    },
+    {
+      icon: {
+        type: "im-icon-Search-onCloud",
+        color: (i) => `${randomColors[getNextCircularIndex(i)]}80`
+      }
+    },
+    {
+      icon: {
+        type: "im-icon-Consulting",
+        color: (i) => `${randomColors[getNextCircularIndex(i)]}80`
+      },
+      content: (i) => <Trans>{t(`home.description.service${i + 1}.content`)}</Trans>
+    },
+    {
+      icon: {
+        type: "im-icon-Sound",
+        color: (i) => `${randomColors[getNextCircularIndex(i)]}80`
+      }
+    }
+  ];
+
+  return <Container>
+    <Title>{TextHelper.capitalize(t('home.services.title'))}</Title>
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-around"
+      alignItems="center"
+    >
+      {details.map((item, i) => {
+        return <Grid key={`detail_${i}`} md={6} xs={12}>
+          <Detail item={{
+            ...item,
+            icon: {
+              ...item.icon,
+              color: item.icon.color(random + i)
+            },
+            content: item.content ? item.content(i) : t(`home.description.service${i + 1}.content`),
+            title: item.title ? item.title(i) : t(`home.description.service${i + 1}.title`),
+          }} />
+        </Grid>
+      })}
+    </Grid>
+  </Container>
+}
+
+export default MotionWrapper(Features, "features");
---
+++ app/components/pageTemplate/Home/Description2/index.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { Container, Divider, Box } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
+import { Title } from "../Typography";
+import { Trans, useTranslation } from "next-i18next";
+import { TextHelper } from "app/common/lib/text";
+import { MotionWrapper } from "app/common/components/hoc";
+import Detail from "./Detail";
+import list from './details.json';
+import { randomColors } from "app/common/constants";
+
+// fixed an issue with ownserState in frid2 mui
+// https://github.com/mui/material-ui/issues/35643#issuecomment-1366206059
+const BoxWrapper = React.forwardRef(function LinkWrapper(props, ref) {
+  // @ts-ignore
+  const { ownerState, ...other } = props;
+  return <Box {...other} ref={ref} />;
+});
+
+type DetailsProp = {
+  icon: {
+    type: string
+    color: string
+  }
+  title?: () => string
+  content?: () => React.ReactNode
+}
+
+const Features = (props) => {
+  const { t } = useTranslation('common');
+  const randomColorLength = randomColors.length;
+  const random = Math.floor(Math.random() * randomColorLength);
+  const getNextCircularIndex = (currentIndex: number): number => {
+    console.log({ currentIndex })
+    return (currentIndex + 1) % randomColorLength;
+  }
+  const details: DetailsProp = [
+    {
+      icon: {
+        type: "im-icon-Clothing-Store",
+        // by adding 80 at the end we are specifying the alpha color value
+        color: (i) => `${randomColors[getNextCircularIndex(i)]}80`
+      }
+    },
+    {
+      icon: {
+        type: "im-icon-Search-onCloud",
+        color: (i) => `${randomColors[getNextCircularIndex(i)]}80`
+      }
+    },
+    {
+      icon: {
+        type: "im-icon-Consulting",
+        color: (i) => `${randomColors[getNextCircularIndex(i)]}80`
+      },
+      content: (i) => <Trans>{t(`home.description.service${i + 1}.content`)}</Trans>
+    },
+    {
+      icon: {
+        type: "im-icon-Sound",
+        color: (i) => `${randomColors[getNextCircularIndex(i)]}80`
+      }
+    }
+  ];
+
+  return <Container>
+    <Title>{TextHelper.capitalize(t('home.services.title'))}</Title>
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-around"
+      alignItems="center"
+    >
+      <Grid md={8} xs={12}>
+        {details.map((item, i) => {
+          return <Detail key={`detail_${i}`} item={{
+            ...item,
+            icon: {
+              ...item.icon,
+              color: item.icon.color(random + i)
+            },
+            content: item.content ? item.content(i) : t(`home.description.service${i + 1}.content`),
+            title: item.title ? item.title(i) : t(`home.description.service${i + 1}.title`),
+          }} />
+        })}
+      </Grid>
+      <Grid
+        component={BoxWrapper}
+        md={4}
+        display={{ sm: "none", md: 'block' }}
+      >
+        image
+      </Grid>
+    </Grid>
+  </Container>
+}
+
+export default MotionWrapper(Features, "features");
